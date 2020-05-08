f27aee438977014831e9e49fa1cb35388416dbed
kaka369 <kaka369@RD-kaka369-MacBookAir.local>
2020-05-09 01:17:18 +0800

    add

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'http://hr.pchomepay.com.tw/',
+        type: 'GET',
+        success: function (response) {
+
+        },
+        complete: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("fromTime").innerHTML = '09 : 00';
-    document.getElementById("toTime").innerHTML = '23 : 00';
+    document.getElementById("toTime").innerHTML = '21 : 00';
 });
