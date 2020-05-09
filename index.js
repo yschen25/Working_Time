d016a2a9d7bbd7c37cefa04bcc9235e7f980aedd
Ka <yschen25@gmail.com>
2020-05-09 11:51:15 +0800

    add

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 $(document).ready(function () {
+
     $.ajax({
-        url: 'http://hr.pchomepay.com.tw/',
+        url: 'https://stackoverflow.com/',
         type: 'GET',
         success: function (response) {
-
+            console.log('success', response);
         },
-        complete: function () {
-
+        complete: function (response) {
+            console.log('complete', response);
         },
-        error: function () {
+        error: function (response) {
 
+            console.log('error', response);
         }
     });
+
 });
+
+
+function findAndReplace() {
+
+    console.log('xxxxx', $('.ta-center').val());
+    $('.ta-center').val('Hello');
+}
+
+window.onload = findAndReplace();
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("fromTime").innerHTML = '09 : 00';
     document.getElementById("toTime").innerHTML = '21 : 00';
 });
+
+
